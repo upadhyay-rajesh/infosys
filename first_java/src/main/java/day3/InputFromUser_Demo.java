eac70b7e0a1e67df98d96da1636a90e25c0bfd5b
Rajesh Upadhyay <rajesh.upadhyay@publicissapient.com>
2021-07-07 15:13:18 +0530

    xgdfg

---
+++ first_java/src/main/java/day3/InputFromUser_Demo.java
@@ -0,0 +1,69 @@
+package day3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class InputFromUser_Demo {
+
+	public static void main(String[] args) {
+		/*Scanner sc=new Scanner(System.in);
+		
+		System.out.println("enter your first no");
+		int i=sc.nextInt();
+		
+		System.out.println("enter your second no");
+		int i1=sc.nextInt();
+		
+		System.out.println("addition is "+(i+i1));
+		*/
+		
+		//what is checked exception?
+		//compile time if system will force to handle some exception then those exception comes under checked exception 
+		//like in below example for br.readLine() it is forcing me to handle IOException so IOException will come under checked exception
+		
+		//what is unchecked exception?
+		//runtime exception is unchecked exception ex-ArithmeticException,NegativeArraySizeException
+		BufferedReader br=null;
+		try {
+		br=new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("enter your first no");
+		String a=br.readLine();
+		int i1=Integer.parseInt(a);
+		
+		if(i1==23) {
+			throw new MyException();
+		}
+		
+		System.out.println("enter your second no");
+		String a1=br.readLine();
+		int i2=Integer.parseInt(a1);
+		
+		System.out.println("division is "+(i1/i2));
+		br.close();
+		}
+		catch(IOException e) {
+			System.out.println("i am io exception "+e);
+			e.printStackTrace();
+		}
+		catch(ArithmeticException e1) {
+			System.out.println("i am arithmetic exception "+e1);
+		}
+		catch(MyException e2) {
+			e2.printStackTrace();
+		}
+		//why finally block?
+		//finally block will insure proper termination of your program means we must close all the resources inside finally block
+		//either try block or catch block is executing but the finally block definitely execute.
+		finally {
+			try {
+				br.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
---
+++ first_java/src/test/java/com/first_java/CalculatorTest1.java
@@ -0,0 +1,39 @@
+package com.first_java;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest1 {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSub() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMult() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDiv() {
+		fail("Not yet implemented");
+	}
+
+}
