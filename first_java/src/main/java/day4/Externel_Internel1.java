635ba640ca850d7be21e770733361ac188a9ba97
Rajesh Upadhyay <rajesh.upadhyay@publicissapient.com>
2021-07-08 16:09:50 +0530

    ccbv

--- first_java/src/main/java/day4/Externel_Internel1.java
+++ first_java/src/main/java/day4/Externel_Internel1.java
@@ -1,4 +1,4 @@
-package stream;
+package day4;
 import java.util.Arrays;
 import java.util.List;
 
---
+++ first_java/src/main/java/day4/Java8Interface_demo.java
@@ -0,0 +1,15 @@
+package day4;
+
+@FunctionalInterface
+public interface Java8Interface_demo {
+	public void add();
+	
+	
+	default void sub() {
+		
+	}
+	
+	static void mult() {
+		
+	}
+}
